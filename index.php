8d1a23dc0a744dd7f089c0379dc37f3597869c0b
pe77 <p321654987@outlook.com>
2013-05-24 19:45:12 -0300

    first tests [ok]

--- index.php
+++ index.php
@@ -11,3 +11,13 @@ $config = Yaml::parse('config.yml');
 // ini
 $programP = new ProgramP($config);
 $bot	  = $programP->GetBot('cenouro');
+$user	  = $programP->GetUser('P.');
+
+$bot->SetProp('name', 'Cenouro');
+$bot->SetProp('gender', 'male');
+$bot->DelProp('nome');
+$bot->Save();
+
+$user->SetProp('name', 'P.');
+$user->SetProp('gender', 'male');
+$user->Save();
