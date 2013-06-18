a8c9428babf118de0ef1bdbcfb59efc30e1f9a16
pe77 <p321654987@outlook.com>
2013-06-18 16:23:32 -0300

    add simple web app for webservice

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
+
 require 'vendor/autoload.php';
 
 use Symfony\Component\Yaml\Yaml;
 use Pe77\ProgramP\ProgramP;
 use Pe77\ProgramP\ProgramP\Classes\Response;
-
 
 // load config
 $config = Yaml::parse('config.yml');
@@ -13,19 +13,15 @@ $config = Yaml::parse('config.yml');
 // ini
 $programP = new ProgramP($config);
 $bot	  = $programP->GetBot('cenouro');
-$user	  = $programP->GetUser('P.');
+$user	  = $programP->GetUser($_SERVER['REMOTE_ADDR']);
 
 $bot->SetProp('name', 'Cenouro');
-$bot->SetProp('gender', 'Male');
-$bot->DelProp('nome');
+$bot->SetProp('gender', 'homem');
 $bot->Save();
 
-$user->SetProp('name', 'P.');
-$user->SetProp('gender', 'male');
+$user->SetProp('nome', $_REQUEST['user']);
 $user->Save();
 
-
-
-$response = $programP->GetResponse($user, $bot, "bot test");
+$response = $programP->GetResponse($user, $bot, $_REQUEST['input']);
 
 echo $response;
